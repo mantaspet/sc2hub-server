da9085ded2ff2f5f095dd8714329ff4975e1a95d
Mantas Petrauskas <mantas.petrauskas96@gmail.com>
2019-03-09 20:40:27 +0200

    Implement event category CRUD methods

--- api/event_categories.go
+++ api/event_categories.go
@@ -1,24 +1,93 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi"
+	"github.com/mantaspet/sc2hub-server/models"
 	"net/http"
 )
 
 func GetEventCategories(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusOK, "get events")
+	var ec models.EventCategory
+	var eventCategories []models.EventCategory
+	rows, err := DB.Query("SELECT id, name, pattern, COALESCE(info_url, '') as info_url, `order` FROM event_categories")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&ec.ID, &ec.Name, &ec.Pattern, &ec.InfoURL, &ec.Order)
+		if err != nil {
+			panic(err)
+		}
+		eventCategories = append(eventCategories, ec)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err.Error())
+	}
+	respondWithJSON(w, http.StatusOK, eventCategories)
 }
 
 func CreateEventCategory(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusOK, "create event")
+	decoder := json.NewDecoder(r.Body)
+	var ec models.EventCategory
+	err := decoder.Decode(&ec)
+	if err != nil {
+		respondWithJSON(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	validation := ec.Validate()
+	if len(validation) > 0 {
+		respondWithJSON(w, http.StatusUnprocessableEntity, validation)
+		return
+	}
+	_, sqlErr := DB.Exec("INSERT INTO event_categories (name, pattern, info_url, `order`) VALUES (?, ?, ?, ?)", ec.Name, ec.Pattern, ec.InfoURL, ec.Order)
+	if sqlErr != nil {
+		respondWithJSON(w, http.StatusInternalServerError, sqlErr)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, "success")
 }
 
 func UpdateEventCategory(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
-	respondWithJSON(w, http.StatusOK, "update event "+id)
+	decoder := json.NewDecoder(r.Body)
+	var ec models.EventCategory
+	err := decoder.Decode(&ec)
+	if err != nil {
+		respondWithJSON(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	validation := ec.Validate()
+	if len(validation) > 0 {
+		respondWithJSON(w, http.StatusUnprocessableEntity, validation)
+		return
+	}
+	res, sqlErr := DB.Exec("UPDATE event_categories SET name=?, pattern=?, info_url=?, `order`=? WHERE id=?", ec.Name, ec.Pattern, ec.InfoURL, ec.Order, id)
+	if sqlErr != nil {
+		respondWithJSON(w, http.StatusInternalServerError, sqlErr)
+		return
+	}
+	rowCnt, _ := res.RowsAffected()
+	if rowCnt == 0 {
+		respondWithJSON(w, http.StatusNotFound, "Event category with specified ID does not exist")
+		return
+	}
+	respondWithJSON(w, http.StatusOK, "success")
 }
 
 func DeleteEventCategory(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
-	respondWithJSON(w, http.StatusOK, "delete event "+id)
+	res, err := DB.Exec("DELETE FROM event_categories WHERE id=?", id)
+	if err != nil {
+		respondWithJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	rowCnt, _ := res.RowsAffected()
+	if rowCnt == 0 {
+		respondWithJSON(w, http.StatusNotFound, "Event category with specified ID does not exist")
+		return
+	}
+	respondWithJSON(w, http.StatusOK, "success")
 }
--- models/event_category.go
+++ models/event_category.go
@@ -1,9 +1,29 @@
 package models
 
 type EventCategory struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Pattern string `json:"pattern"`
-	InfoURL string `json:"info_url"`
-	Order   int    `json:"order"`
+	ID      int
+	Name    string
+	Pattern string
+	InfoURL string
+	Order   int
+}
+
+func (ec EventCategory) Validate() map[string]string {
+	errors := make(map[string]string)
+	if len(ec.Name) < 1 {
+		errors["Name"] = "Field is required"
+	}
+	if len(ec.Pattern) < 1 {
+		errors["Pattern"] = "Field is required"
+	}
+	if ec.Order == 0 {
+		errors["Order"] = "Field is required"
+	}
+	if ec.Order < 0 {
+		errors["Order"] = "Order must be above zero"
+	}
+	if len(errors) > 0 {
+		return errors
+	}
+	return nil
 }
