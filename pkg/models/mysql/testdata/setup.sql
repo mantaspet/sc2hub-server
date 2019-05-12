802cfaea19805970c5038d87e9609506b03b5332
mantas <mantas.petrauskas96@gmail.com>
2019-05-12 10:31:01 +0300

    update db setup and teardown scripts

--- pkg/models/mysql/testdata/setup.sql
+++ pkg/models/mysql/testdata/setup.sql
@@ -15,8 +15,8 @@ create or replace table event_categories
 (
     id int unsigned auto_increment
         primary key,
-    name varchar(255) not null,
-    pattern varchar(255) not null,
+    name varchar(191) not null,
+    pattern varchar(191) not null,
     info_url text default '' not null,
     image_url text default '' not null,
     description text default '' not null,
@@ -84,62 +84,62 @@ create or replace table event_category_channels
         foreign key (event_category_id) references event_categories (id)
 );
 
-# create or replace table players
-# (
-#     id int unsigned auto_increment
-#         primary key,
-#     player_id varchar(255) not null,
-#     name varchar(255) default '' not null,
-#     race varchar(8) not null,
-#     team varchar(255) default '' not null,
-#     country varchar(255) default '' not null,
-#     total_earnings decimal default 0 not null,
-#     date_of_birth date null,
-#     liquipedia_url text default '' not null,
-#     image_url text default '' not null,
-#     stream_url text default '' not null,
-#     is_retired tinyint(1) default 0 not null,
-#     constraint players_player_id_uindex
-#         unique (player_id)
-# );
+create or replace table players
+(
+    id int unsigned auto_increment
+        primary key,
+    player_id varchar(191) not null,
+    name varchar(255) default '' not null,
+    race varchar(8) not null,
+    team varchar(255) default '' not null,
+    country varchar(255) default '' not null,
+    total_earnings decimal default 0 not null,
+    date_of_birth date null,
+    liquipedia_url text default '' not null,
+    image_url text default '' not null,
+    stream_url text default '' not null,
+    is_retired tinyint(1) default 0 not null,
+    constraint players_player_id_uindex
+        unique (player_id)
+);
 
-# create or replace table player_articles
-# (
-#     player_id int unsigned not null,
-#     article_id int unsigned not null,
-#     primary key (player_id, article_id),
-#     constraint player_articles_article_id_fk
-#         foreign key (article_id) references articles (id),
-#     constraint player_articles_player_id_fk
-#         foreign key (player_id) references players (id)
-# );
+create or replace table player_articles
+(
+    player_id int unsigned not null,
+    article_id int unsigned not null,
+    primary key (player_id, article_id),
+    constraint player_articles_article_id_fk
+        foreign key (article_id) references articles (id),
+    constraint player_articles_player_id_fk
+        foreign key (player_id) references players (id)
+);
 
-# create or replace table videos
-# (
-#     id varchar(64) not null
-#         primary key,
-#     event_category_id int unsigned null,
-#     platform_id int unsigned not null,
-#     channel_id varchar(64) null,
-#     title varchar(255) not null,
-#     duration varchar(16) default '' not null,
-#     thumbnail_url text default '' not null,
-#     created_at datetime default current_timestamp() not null,
-#     type varchar(64) default '' null,
-#     constraint videos_channels_id_fk
-#         foreign key (channel_id) references channels (id)
-#             on delete set null,
-#     constraint videos_event_categories_id_fk
-#         foreign key (event_category_id) references event_categories (id)
-# );
+create or replace table videos
+(
+    id varchar(64) not null
+        primary key,
+    event_category_id int unsigned null,
+    platform_id int unsigned not null,
+    channel_id varchar(64) null,
+    title varchar(255) not null,
+    duration varchar(16) default '' not null,
+    thumbnail_url text default '' not null,
+    created_at datetime default current_timestamp() not null,
+    type varchar(64) default '' null,
+    constraint videos_channels_id_fk
+        foreign key (channel_id) references channels (id)
+            on delete set null,
+    constraint videos_event_categories_id_fk
+        foreign key (event_category_id) references event_categories (id)
+);
 
-# create table player_videos
-# (
-#     player_id int unsigned not null,
-#     video_id varchar(64) not null,
-#     primary key (player_id, video_id),
-#     constraint player_videos_players_id_fk
-#         foreign key (player_id) references players (id),
-#     constraint player_videos_videos_id_fk
-#         foreign key (video_id) references videos (id)
-# );
+create table player_videos
+(
+    player_id int unsigned not null,
+    video_id varchar(64) not null,
+    primary key (player_id, video_id),
+    constraint player_videos_players_id_fk
+        foreign key (player_id) references players (id),
+    constraint player_videos_videos_id_fk
+        foreign key (video_id) references videos (id)
+);
--- pkg/models/mysql/testdata/teardown.sql
+++ pkg/models/mysql/testdata/teardown.sql
@@ -1,10 +1,10 @@
-# drop table player_videos;
-# drop table player_articles;
+drop table player_videos;
+drop table player_articles;
 drop table event_category_articles;
 drop table event_category_channels;
-# drop table videos;
+drop table videos;
 drop table articles;
-# drop table players;
+drop table players;
 drop table events;
 drop table channels;
 drop table event_categories;
